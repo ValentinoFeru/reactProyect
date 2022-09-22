d3e7c42849519a5b7da8da302ce6d82bba603739
ValentinoFeru <valmaster16@gmail.com>
2022-09-21 22:29:28 -0300

    newnewnewnew

--- src/components/ItemListContainer/ItemList.jsx
+++ src/components/ItemListContainer/ItemList.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
 import Item from './Item'
+import styled from 'styled-components'
+import { screen } from '../../constants/screen';
 
 const ItemList = ({data = []}) => {
   return (
-    data.map(bebida => <Item key={bebida.id} info={bebida} />)
+    <Greet>
+    <div className='ganga'>
+      {data.map(bebida => <Item key={bebida.id} info={bebida} />)}
+    </div>
+    </Greet>
   )
 }
 
 export default ItemList
+
+const Greet = styled.div`
+.ganga{
+    display: grid;
+    grid-template-columns: repeat(1, 1fr);
+    margin: auto;
+}
+
+
+@media (min-width: ${screen.sm}px) {
+    .ganga{
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    
+}
+}
+
+@media (min-width: ${screen.md}px) {
+    .ganga{
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+}
+}
+
+@media (min-width: ${screen.lg}px) {
+    .ganga{
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+}
+}
+`
